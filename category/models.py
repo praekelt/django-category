8eed1e0c5ea8e779ea89802d115c8bd02b8532c4
Shaun Sephton <shaun@praekelt.com>
2010-04-16 13:05:50 +0200

    added unicode and meta

--- category/models.py
+++ category/models.py
@@ -5,3 +5,11 @@ class Category(models.Model):
         max_length=64,
         help_text='Short descriptive name for this category.',
     )
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
