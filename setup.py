ed019b3b9181727db77453ecc181a48adffe0c27
shaunsephton <shaunsephton@gmail.com>
2010-06-14 12:08:59 +0200

    polish

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-category',
-    version='dev',
-    description='Django categorize app.',
-    author='Praekelt Consulting',
+    version='0.0.1',
+    description='Django categorize content app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-category',
+    license='BSD',
+    url='http://github.com/praekelt/django-category',
     packages = find_packages(),
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
