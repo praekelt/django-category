a59ed134d5692ec10ac65b4447d3c1a291951292
shaunsephton <shaunsephton@gmail.com>
2010-07-09 15:17:43 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-category',
-    version='0.0.1',
+    version='0.0.2',
     description='Django categorize content app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
