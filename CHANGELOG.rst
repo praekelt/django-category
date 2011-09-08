8d1799137f429a3b3cb31e1b8256b0084486a1b6
hedleyroos <hedleyroos@gmail.com>
2011-09-08 11:56:41 +0200

    Use prepopulate_fields for admin

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+0.0.5
+-----
+#. Use prepopulate_fields for admin interface
+
 0.0.4 (2011-08-24)
 ------------------
 #. Docs, testrunner.
--- category/admin.py
+++ category/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from category.models import Category, Tag
 
-admin.site.register(Category)
-admin.site.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
