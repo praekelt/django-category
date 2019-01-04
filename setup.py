2fdefa6e2a61d3faa2888bd991b9ff59c9bc185e
cblignaut <cilliers@praekeltconsulting.com>
2019-01-04 10:45:50 +0200

    Update setup.py file

--- setup.py
+++ setup.py
@@ -4,25 +4,37 @@ setup(
     name="django-category",
     version="2.0.0",
     description="Django categorize content app.",
-    long_description = open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
+    long_description=open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
     author="Praekelt Consulting",
     author_email="dev@praekelt.com",
     license="BSD",
     url="http://github.com/praekelt/django-category",
     packages=find_packages(),
     include_package_data=True,
-    install_requires = [
-        "django>=1.10",
-        "django-object-tools>=1.9",
+    install_requires=[
+        "django>=1.11",
     ],
+    tests_require=[
+        "tox"
+    ],
+    test_suite="tox",
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Development Status :: 5 - Production/Stable",
         "Operating System :: OS Independent",
-        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
     zip_safe=False,
 )
